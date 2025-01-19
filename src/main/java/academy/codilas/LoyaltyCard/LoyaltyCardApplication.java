926510702b128df37bde075c0a83fdd3982cec31
Oleksandr Honcharenko <axelwerst@gmail.com>
2025-01-19 13:19:20 +0100

    lo

---
+++ src/main/java/academy/codilas/LoyaltyCard/LoyaltyCardApplication.java
@@ -0,0 +1,14 @@
+package academy.codilas.loyaltycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyCardApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/academy/codilas/LoyaltyCard/LoyaltyCardApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.codilas.LoyaltyCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
